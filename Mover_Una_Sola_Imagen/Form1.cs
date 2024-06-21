63196a25e3731ac0fa32d7155f17be62678821b5
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-20 23:37:50 -0400

    Eventos agregados a la interfaz!

--- Mover_Una_Sola_Imagen/Form1.cs
+++ Mover_Una_Sola_Imagen/Form1.cs
@@ -6,5 +6,33 @@ namespace Mover_Una_Sola_Imagen
         {
             InitializeComponent();
         }
+
+        private void EventoPresionarTeclas(object sender, KeyEventArgs e)
+        {
+            // EVENTO PARA PRESIONAR UNA TECLA DEL TECLADO MEDIANTE LA INTERFAZ...
+
+            // EN INSTANTES...
+        }
+
+        private void EventoSoltarTeclas(object sender, KeyEventArgs e)
+        {
+            // EVENTO PARA SOLTAR UNA TECLA DEL TECLADO MEDIANTE LA INTERFAZ...
+
+            // EN INSTANTES...
+        }
+
+        private void EventoDiseñoFormulario(object sender, PaintEventArgs e)
+        {
+            // EVENTO PARA DISEÑAR LA INTERFAZ DEL PROYECTO MEDIANTE RECURSOS IMPORTADOS...
+
+            // EN INSTANTES...
+        }
+
+        private void EventoTemporizador(object sender, EventArgs e)
+        {
+            // EVENTO EJECUTABLE MEDIANTE UN TEMPORIZADOR...
+
+            // EN INSTANTES...
+        }
     }
 }
