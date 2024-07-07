a0e17865064e88146f9959e305063f0031412bf1
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-06 23:30:07 -0400

    Método para presionar las teclas completado.

--- Mover_Una_Sola_Imagen/Form1.cs
+++ Mover_Una_Sola_Imagen/Form1.cs
@@ -2,6 +2,7 @@ namespace Mover_Una_Sola_Imagen
 {
     public partial class Form1 : Form
     {
+        bool izquierda, derecha, arriba, abajo;
         public Form1()
         {
             InitializeComponent();
@@ -11,7 +12,27 @@ namespace Mover_Una_Sola_Imagen
         {
             // EVENTO PARA PRESIONAR UNA TECLA DEL TECLADO MEDIANTE LA INTERFAZ...
 
-            // EN INSTANTES...
+            // PROCEDIMIENTO SENCILLO POR TECLAS...
+
+            if (e.KeyCode == Keys.Left) // SI SE PRESIONA LA TECLA DE IZQUIERDA...
+            {
+                izquierda = true; // LA IMAGEN SE MUEVE HACIA LA IZQUIERDA.
+            }
+
+            if (e.KeyCode == Keys.Right) // SI SE PRESIONA LA TECLA DE DERECHA...
+            {
+                derecha = true; // LA IMAGEN SE MUEVE HACIA LA DERECHA.
+            }
+
+            if (e.KeyCode == Keys.Up) // SI SE PRESIONA LA TECLA DE ARRIBA...
+            {
+                arriba = true; // LA IMAGEN SE MUEVE HACIA ARRIBA.
+            }
+
+            if (e.KeyCode == Keys.Down) // SI SE PRESIONA LA TECLA DE ABAJO...
+            {
+                abajo = true; // LA IMAGEN SE MUEVE HACIA ABAJO.
+            }
         }
 
         private void EventoSoltarTeclas(object sender, KeyEventArgs e)
