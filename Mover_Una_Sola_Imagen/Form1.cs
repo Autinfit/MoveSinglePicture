38719605566de6ed5358e59fc7521ff5f4bf70c2
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-07 23:50:56 -0400

    Evento de soltar teclas concluido!

--- Mover_Una_Sola_Imagen/Form1.cs
+++ Mover_Una_Sola_Imagen/Form1.cs
@@ -39,7 +39,27 @@ namespace Mover_Una_Sola_Imagen
         {
             // EVENTO PARA SOLTAR UNA TECLA DEL TECLADO MEDIANTE LA INTERFAZ...
 
-            // EN INSTANTES...
+            // MISMO PROCEDIMIENTO PERO SOLTANDO CUALQUIER TECLA DEL TECLADO...
+
+            if (e.KeyCode == Keys.Left) // SI SE SUELTA LA TECLA DE IZQUIERDA...
+            {
+                izquierda = false; // LA IMAGEN NO SE MUEVE HACIA LA IZQUIERDA.
+            }
+
+            if (e.KeyCode == Keys.Right) // SI SE SUELTA LA TECLA DE DERECHA...
+            {
+                derecha = true; // LA IMAGEN NO SE MUEVE HACIA LA DERECHA.
+            }
+
+            if (e.KeyCode == Keys.Up) // SI SE SUELTA LA TECLA DE ARRIBA...
+            {
+                arriba = true; // LA IMAGEN NO SE MUEVE HACIA ARRIBA.
+            }
+
+            if (e.KeyCode == Keys.Down) // SI SE SUELTA LA TECLA DE ABAJO...
+            {
+                abajo = true; // LA IMAGEN NO SE MUEVE HACIA ABAJO.
+            }
         }
 
         private void EventoDiseñoFormulario(object sender, PaintEventArgs e)
